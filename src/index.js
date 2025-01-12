06b53744b1ea6cd9b368c18a581c027862b81be6
Bharat Kumar <100489720+bharatkumarmali@users.noreply.github.com>
2025-01-12 22:56:08 +0530

    add config in app.js and add utils

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import mongoose from "mongoose"
 import { DB_NAME } from "./constants.js"
+
 import connectDB from "./db/index.js"
 import dotenv from "dotenv"
+import app from "./app.js"
 
 dotenv.config({
     path: "./.env"
 });
 
-connectDB();
 
+
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERROR IN SERVER SETUP : ", error)
+            throw error;
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log("database connection error : ", error)
+    })
 
 
 
