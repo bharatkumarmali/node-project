751b3f44581075adca49732cb41156c066131efd
Bharat Kumar <100489720+bharatkumarmali@users.noreply.github.com>
2025-01-18 15:29:05 +0530

    Refactor logoutUser function to use $unset for refreshToken removal in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -172,8 +172,11 @@ const logoutUser = asyncHandler(async (req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            // $set: {
+            //     refreshToken: undefined
+            // }
+            $unset: {
+                refreshToken: 1
             }
         },
         {
